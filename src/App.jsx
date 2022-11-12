561181b81d72b885b66a671f9c8da2a4d61ae75c
felipenmatos <notfelipe2@hotmail.com>
2022-11-12 19:16:18 -0300

    feat: base page structuring

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <Grid container>
+      <Header />
+    </Grid>
+  );
+}
+
+export default App;
